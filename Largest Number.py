fc5697d36183741efe8d7afa2809fbd04672f9ba
Lisanegebriel Abay <155144454+lisan-5@users.noreply.github.com>
2024-04-15 20:50:43 +0300

    Create Largest Number.py

---
+++ Largest Number.py
@@ -0,0 +1,4 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = sorted(map(str, nums), key=lambda x: x * 10, reverse=True)
+        return ''.join(nums) if nums[0] != '0' else '0'
