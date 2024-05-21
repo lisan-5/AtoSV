597cb21edc88a17724f77736d610199b560e1763
Lisanegebriel Abay <155144454+lisan-5@users.noreply.github.com>
2024-05-21 21:08:43 +0300

    Create Design Circular Deque.py

---
+++ Design Circular Deque.py
@@ -0,0 +1,53 @@
+class MyCircularDeque:
+    def __init__(self, k: int):
+        self.k = k
+        self.queue = [0] * k
+        self.front = 0
+        self.rear = 0
+        self.size = 0
+
+    def insertFront(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.front = (self.front - 1 + self.k) % self.k
+        self.queue[self.front] = value
+        self.size += 1
+        return True
+
+    def insertLast(self, value: int) -> bool:
+        if self.isFull():
+            return False
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.k
+        self.size += 1
+        return True
+
+    def deleteFront(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.front = (self.front + 1) % self.k
+        self.size -= 1
+        return True
+
+    def deleteLast(self) -> bool:
+        if self.isEmpty():
+            return False
+        self.rear = (self.rear - 1 + self.k) % self.k
+        self.size -= 1
+        return True
+
+    def getFront(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[self.front]
+
+    def getRear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[(self.rear - 1 + self.k) % self.k]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.k
