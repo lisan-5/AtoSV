4d4be47da7e801b7df9b38be3001b581cda592d5
Lisanegebriel Abay <155144454+lisan-5@users.noreply.github.com>
2024-06-20 15:59:18 +0300

    Create Intersection of Two Arrays II.py

---
+++ Intersection of Two Arrays II.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1, nums2):
+        count1 = Counter(nums1)
+        count2 = Counter(nums2)
+        intersection = []
+        for num in count1:
+            if num in count2:
+                intersection.extend([num] * min(count1[num], count2[num]))    
+        return intersection
