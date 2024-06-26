5e15f11929cb60f96cd44a3937cb277e1b139ddd
Lisanegebriel Abay <155144454+lisan-5@users.noreply.github.com>
2024-06-26 23:22:05 +0300

    Create 3Sum Closest .py

---
+++ 3Sum Closest .py
@@ -0,0 +1,18 @@
+class Solution:
+    def threeSumClosest(self, nums, target):
+        nums.sort()
+        closest_sum = float('inf')
+        
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if abs(total - target) < abs(closest_sum - target):
+                    closest_sum = total
+                if total < target:
+                    left += 1
+                elif total > target:
+                    right -= 1
+                else:
+                    return total
+        return closest_sum
