fdafbd3f321b160580849ad9d515afe2a4efcb4d
Lisanegebriel Abay <155144454+lisan-5@users.noreply.github.com>
2024-07-10 18:15:14 +0300

    Create Binary Tree Right Side View.py

---
+++ Binary Tree Right Side View.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        right_view = []
+        queue = [root]
+        
+        while queue:
+            level_length = len(queue)
+            for i in range(level_length):
+                node = queue.pop(0)
+                if i == level_length - 1:
+                    right_view.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return right_view
